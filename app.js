c39b2ef27e0439c6dec11ee3db1c46b6df6223f9
Albert Ferreira <albert@espacow.com.br>
2020-11-04 21:28:47 -0300

    create app file main

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const http = require('http');
   
-
-
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
